7c3de888b73415b21d560357ed9b6fb15c7c8083
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-03-22 15:15:26 -0300

    Use CMS

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,16 +6,12 @@ import blogStyles from "./blog.module.scss"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
@@ -26,11 +22,11 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => (
-          <li className={blogStyles.post} key={edge.node.frontmatter.title}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2> {edge.node.frontmatter.title} </h2>
-              <p>{edge.node.frontmatter.date}</p>
+        {data.allContentfulBlogPost.edges.map(edge => (
+          <li className={blogStyles.post} key={edge.node.title}>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2> {edge.node.title} </h2>
+              <p>{edge.node.publishedDate}</p>
             </Link>
           </li>
         ))}
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,15 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          title
+          file {
+            url
+          }
+        }
       }
-      html
     }
   }
 `
@@ -17,11 +24,11 @@ export const query = graphql`
 const Blog = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(
+        JSON.parse(props.data.contentfulBlogPost.body.raw)
+      )}
     </Layout>
   )
 }
