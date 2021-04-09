b182d00e3d4d1e8d2eddd7485d25494ba7b48a03
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-04-09 13:02:30 -0300

    Add example of calling an API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
 
-const IndexPage = () => (
-  <Layout>
-    <Head title="Home" />
-    <h1>Hello!</h1>
-    <h2>I'm Rafael, and this is me learning Gatsby.</h2>
-    <p>
-      Need a developer? <Link to="/contact">Contact me</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [fetchedData, setFetchedData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://random-data-api.com/api/dessert/random_dessert?size=15")
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setFetchedData(json);
+        setLoading(false);
+      })
+  }, [])
+
+  return (
+    <Layout>
+      <Head title="Home" />
+      <h1>Hello!</h1>
+      <h2>I'm Rafael, and this is me learning Gatsby.</h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>
+      </p>
+
+      {/*  Calling an API */}
+      <hr />
+      <p>This are some posts I just fetched from an API:</p>
+      {loading ? <p>Loading API data...</p> : (
+      <ul>
+        {fetchedData.map(item => {
+          return <li key={item.id}>{item.variety}</li>
+        })}
+      </ul> )}
+    </Layout>
+  )
+}
 
 export default IndexPage
